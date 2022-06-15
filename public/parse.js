b2579a389eb54b202cd31c917e3581428b14d7d1
Sam Washington <sam@spwashi.com>
2022-06-14 23:24:41 -0500

    milestone: added generator

--- public/parse.js
+++ public/parse.js
@@ -5,26 +5,48 @@ const FatalError = class extends Error {
 let ERROR_MUST_BE_STRING   = 'Input must be a string';
 let ERROR_CANNOT_BE_STRING = 'Cannot parse strings';
 
+function assertStringInput(input) {
+  if (typeof input !== 'string' || !input.length) {
+    throw new FatalError(ERROR_MUST_BE_STRING)
+  }
+}
+
 /**
  * Parser for the Spw Language
  *
  *
  * @param input {string} The input string to parse
+ * @param asGenerator {boolean?} Whether to loop through the generator or return it.
  */
-function parse(input) {
-  const stringLength = input.length;
-  if (typeof input !== 'string' || !stringLength) {
-    throw new FatalError(ERROR_MUST_BE_STRING)
+function parse(input, asGenerator = false) {
+  assertStringInput(input);
+
+  function* loop() {
+    let i = 0;
+    while (i < input.length) {
+      yield input[i];
+      i++;
+    }
   }
 
-  throw new FatalError(ERROR_CANNOT_BE_STRING);
+  if (asGenerator) {
+    return loop();
+  }
+
+  let out = [];
+  for (let v of loop()) {
+    console.log(v);
+    out.push(v);
+  }
+  return out;
 }
 
 function test() {
-  __assertCannotBeString();
+  __assertMustBeString();
+
   return true;
 
-  function __assertCannotBeString() {
+  function __assertMustBeString() {
     let error;
     try {
       parse(2)
